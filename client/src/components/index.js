b7b483d205b648ad9224c6866c1715aed49ec1f1
Alan Daniel Alvarez Castro <alandanielalvarez0000@gmail.com>
2023-09-13 01:49:50 -0600

    feat: add index.js to components folder

---
+++ client/src/components/index.js
@@ -0,0 +1,6 @@
+export { default as TickIcon } from './TickIcon';
+export { default as ProgressBar } from './ProgressBar';
+export { default as Auth } from './Auth';
+export { default as ListHeader } from './ListHeader';
+export { default as ListItem } from './ListItem';
+export { default as Modal } from './Modal';
