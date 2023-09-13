9dcfcc76eef10bec92f8feb4c5935748dbab73aa
Alan Daniel Alvarez Castro <alandanielalvarez0000@gmail.com>
2023-09-13 01:12:15 -0600

    feat: added db and server files into server side
    aldo added /todos test endpoint to check the postgresql conection

---
+++ server/server.js
@@ -0,0 +1,18 @@
+const PORT = process.env.PORT ?? 8000 
+const express = require('express')
+const app = express()
+const pool = require('./db')
+
+
+// Get all todos 
+app.get('/todos', async (req, res) => {
+    const userEmail = 'alanbrito@yopmail.com'
+    try {
+        const todos = await pool.query('SELECT * FROM todos WHERE user_email = $1', [userEmail])
+        res.json(todos.rows) 
+    } catch (error) {
+        console.error(error)
+    }
+})
+
+app.listen(PORT, ()=> console.log(`Server runing on PORT: ${PORT}`))
