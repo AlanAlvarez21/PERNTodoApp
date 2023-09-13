d57d02a0c046df1ce9b7c303c78ac95852aa4e0e
Alan Daniel Alvarez Castro <alandanielalvarez0000@gmail.com>
2023-09-13 02:25:09 -0600

    feat: implement CORS into the app

--- server/server.js
+++ server/server.js
@@ -1,12 +1,14 @@
 const PORT = process.env.PORT ?? 8000 
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const pool = require('./db')
 
+app.use(cors())
 
 // Get all todos 
-app.get('/todos', async (req, res) => {
-    const userEmail = 'alanbrito@yopmail.com'
+app.get('/todos/:userEmail', async (req, res) => {
+    const { userEmail } = req.params
     try {
         const todos = await pool.query('SELECT * FROM todos WHERE user_email = $1', [userEmail])
         res.json(todos.rows) 
